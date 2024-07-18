5534de8274bb712e49339f84e9520237a082b1cf
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-18 15:58:56 -0700

    Using FormData to extract elements from <form>
    We can use FormData(e.currentTarget) to extract the event from the <form> element. However, in order for handleSubmit's prop e to be valid. We need to pass in generic type <HTMLFormElement>

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,18 +1,20 @@
 import React, { FormEvent } from "react";
 
 export default function NewGoal() {
-  function handleSubmit(e: FormEvent) {
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    new FormData(e.currentTarget);
   }
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
       <p>
         <label htmlFor="goal">Your Goal</label>
-        <input id="goal" type="text"></input>
+        <input id="goal" type="text" name="goal"></input>
       </p>
       <p>
         <label htmlFor="summary">Short Summary</label>
-        <input id="summary" type="text"></input>
+        <input id="summary" type="text" name="summary"></input>
       </p>
       <p>
         <button>Add Goal</button>
