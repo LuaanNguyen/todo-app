4e7f73d3e4e6470502c9f03dc2329899908287c9
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-18 11:45:41 -0700

    Create CourseGoal.tsx 🏹

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CourseGoal(props) {
+  return (
+    <article>
+      <div>
+        <h2>Title</h2>
+        <p>Description </p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
