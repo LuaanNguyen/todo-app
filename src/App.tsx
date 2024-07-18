d34c570192d4b1a1ca70e738282915e03c507cde
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-18 12:16:10 -0700

    Defining a Type for Props with "children"
    Learned about ReactNode and PropsWithChildren<{}>

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import CourseGoal from "./components/CourseGoal.tsx";
 function App() {
   return (
     <main>
-      <CourseGoal
-        title="Learn ReactTS"
-        description="Learn React and TypeScript from the ground up."
-      />
+      <CourseGoal title="Learn ReactTS">
+        <p>Learn React and TypeScript from the ground up.</p>
+      </CourseGoal>
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { type PropsWithChildren, type ReactNode } from "react";
 
 // type CourseGoalProps = {
 //   title: string;
 //   description: string;
 // };
 
-interface CourseGoalProps {
-  title: string;
-  description: string;
-}
+//ReactNode for {children}
 
-export default function CourseGoal({ title, description }: CourseGoalProps) {
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>; // (same as ReactNode)
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <p>{description}</p>
+        {children}
       </div>
       <button>Delete</button>
     </article>
