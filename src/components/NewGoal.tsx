bb44f91d8121e6a52dfadb6783e02e870f155ea6
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-18 16:13:04 -0700

    replaced FormData with useRef
    We can use useRef to access elements in <form>

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,20 +1,26 @@
 import React, { FormEvent } from "react";
+import { useRef } from "react";
 
 export default function NewGoal() {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    new FormData(e.currentTarget);
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
+    console.log(enteredGoal, enteredSummary);
   }
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
       <p>
         <label htmlFor="goal">Your Goal</label>
-        <input id="goal" type="text" name="goal"></input>
+        <input id="goal" type="text" ref={goal}></input>
       </p>
       <p>
         <label htmlFor="summary">Short Summary</label>
-        <input id="summary" type="text" name="summary"></input>
+        <input id="summary" type="text" ref={summary}></input>
       </p>
       <p>
         <button>Add Goal</button>
