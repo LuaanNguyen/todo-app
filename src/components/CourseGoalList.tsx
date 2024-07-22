d1d907eb55ec8dcb35831683a7388b19279ae0f0
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-21 20:16:08 -0700

    Building a More Dynamic & Flexible Component
    Added dynamically generated <InfoBox/> components to check whether the user has no goals yet or has a lot of goals. And it will display different message based on the number of goals.

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal";
 import { type CourseGoal as CGoal } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type CourseGoalProps = {
   goals: CGoal[];
@@ -10,19 +12,40 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goal yet. Start adding some.
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You are collecting a lot of responsibilities. Don't put too much on your
+        plate!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal
-            id={goal.id}
-            title={goal.title}
-            onDeleteGoal={onDeleteGoal}
-          >
-            {goal.description}
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal
+              id={goal.id}
+              title={goal.title}
+              onDeleteGoal={onDeleteGoal}
+            >
+              {goal.description}
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
